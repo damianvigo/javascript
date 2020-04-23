1b29ae9bad2d5e064509875644607b3f95566dc9
Damian <damiandanielvigo@gmail.com>
2020-04-22 23:50:04 -0300

    22

--- logica/logica.js
+++ logica/logica.js
@@ -456,7 +456,7 @@ const devolverCuadrados = (arr = undefined) => {
 
   const newArr = arr.map(el => el * el);
 
-  return console.info(`Arreglo original: ${arr},\n Arreglo elevado al cuadrado: ${newArr}`)
+  return console.info(`Arreglo original: ${arr},\nArreglo elevado al cuadrado: ${newArr}`)
 
 };
 
@@ -469,5 +469,29 @@ devolverCuadrados([1, 4, {}]);
 devolverCuadrados([1, 4, 8]);
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+const arrayMinMax = (arr = undefined) => {
+
+  if (arr === undefined) return console.warn('No ingresaste un array de números');
+
+  if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
+
+  if (arr.length === 0) return console.error('El array esta vacío');
+
+  for (let num of arr) {
+    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`);
+  }
+
+  return console.info(`Array original: ${arr}\nValor mayor: ${Math.max(...arr)}, \nValor menor: ${Math.min(...arr)}`);
+
+}
+
+arrayMinMax();
+arrayMinMax(false);
+arrayMinMax([]);
+arrayMinMax([2, 3, true]);
+arrayMinMax([1, 4, 5, 99, -60])
+
+
+
 
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
