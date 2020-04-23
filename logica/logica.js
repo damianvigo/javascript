3ad2e8727fc43464e71a227bce3c434810564c9b
Damian <damiandanielvigo@gmail.com>
2020-04-23 00:12:21 -0300

    23

--- logica/logica.js
+++ logica/logica.js
@@ -491,7 +491,27 @@ arrayMinMax([]);
 arrayMinMax([2, 3, true]);
 arrayMinMax([1, 4, 5, 99, -60])
 
-
-
-
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+
+const separarParesImpares = (arr = undefined) => {
+  if (arr === undefined) return console.warn('No ingresaste un array de números');
+
+  if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
+
+  if (arr.length === 0) return console.error('El array esta vacío');
+
+  for (let num of arr) {
+    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`);
+  }
+
+  return console.info({
+    pares: arr.filter(num => num % 2 === 0),
+    impares: arr.filter(num => num % 2 === 1)
+  })
+}
+
+separarParesImpares();
+separarParesImpares('hola');
+separarParesImpares([]);
+separarParesImpares([2, 4, "ja"]);
+separarParesImpares([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
