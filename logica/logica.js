9f0e38913cebf6e9a7cc48422014a48de761db6e
Damian <damiandanielvigo@gmail.com>
2020-05-02 21:01:09 -0300

    24-25-26

--- logica/logica.js
+++ logica/logica.js
@@ -451,26 +451,24 @@ const devolverCuadrados = (arr = undefined) => {
   if (arr.length === 0) return console.error('El array esta vacío');
 
   for (let num of arr) {
-    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`)
+    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`);
   }
 
-  const newArr = arr.map(el => el * el);
+  const newArr = arr.map((el) => el * el);
 
-  return console.info(`Arreglo original: ${arr},\nArreglo elevado al cuadrado: ${newArr}`)
-
+  return console.info(`Arreglo original: ${arr},\nArreglo elevado al cuadrado: ${newArr}`);
 };
 
 devolverCuadrados();
 devolverCuadrados(true);
 devolverCuadrados({});
 devolverCuadrados([]);
-devolverCuadrados([1, "3", 4, {}]);
+devolverCuadrados([1, '3', 4, {}]);
 devolverCuadrados([1, 4, {}]);
 devolverCuadrados([1, 4, 8]);
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
 const arrayMinMax = (arr = undefined) => {
-
   if (arr === undefined) return console.warn('No ingresaste un array de números');
 
   if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
@@ -482,14 +480,13 @@ const arrayMinMax = (arr = undefined) => {
   }
 
   return console.info(`Array original: ${arr}\nValor mayor: ${Math.max(...arr)}, \nValor menor: ${Math.min(...arr)}`);
-
-}
+};
 
 arrayMinMax();
 arrayMinMax(false);
 arrayMinMax([]);
 arrayMinMax([2, 3, true]);
-arrayMinMax([1, 4, 5, 99, -60])
+arrayMinMax([1, 4, 5, 99, -60]);
 
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
 
@@ -505,13 +502,129 @@ const separarParesImpares = (arr = undefined) => {
   }
 
   return console.info({
-    pares: arr.filter(num => num % 2 === 0),
-    impares: arr.filter(num => num % 2 === 1)
-  })
-}
+    pares: arr.filter((num) => num % 2 === 0),
+    impares: arr.filter((num) => num % 2 === 1),
+  });
+};
 
 separarParesImpares();
 separarParesImpares('hola');
 separarParesImpares([]);
-separarParesImpares([2, 4, "ja"]);
+separarParesImpares([2, 4, 'ja']);
 separarParesImpares([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
+// 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.
+const ordenarArray = (arr = 'undefined') => {
+  if (arr === undefined) return console.warn('No ingresaste un array de números');
+
+  if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
+
+  if (arr.length === 0) return console.error('El array esta vacío');
+
+  for (let num of arr) {
+    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`);
+  }
+
+  return console.info({
+    arr,
+    asc: arr.map(function (el) {
+      return el
+    }).sort(), // en forma ascendente
+    des: arr.map(el => el).sort().reverse()
+  });
+};
+
+ordenarArray();
+ordenarArray('Hola');
+ordenarArray([]);
+ordenarArray([3, {}]);
+ordenarArray([7, 5, 7, 8, 6]);
+
+// 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true].
+const quitarDuplicados = (arr = undefined) => {
+  if (arr === undefined) return console.warn('No ingresaste un array de números');
+
+  if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
+
+  if (arr.length === 0) return console.error('El array esta vacío');
+
+  if (arr.length === 1) return console.warn('El arreglo debe tener al menos 2 elementos');
+
+  // return console.info({
+  //   original: arr,
+  //   SinDuplicados: arr.filter((value, index, self) => self.indexOf(value) === index)
+  // })
+  return console.info({
+    original: arr,
+    SinDuplicados: [...new Set(arr)] // Set recibe el array y va iterando creando un conjunto de elementos pero que no se repitan entre si.
+  })
+}
+
+quitarDuplicados();
+quitarDuplicados({});
+quitarDuplicados([]);
+quitarDuplicados([2]);
+quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true]);
+
+// 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+const promedio = (arr = undefined) => {
+  if (arr === undefined) return console.warn('No ingresaste un array de números');
+
+  if (!(arr instanceof Array)) return console.error('El valor ingresado no es un array');
+
+  if (arr.length === 0) return console.error('El array esta vacío');
+
+  for (let num of arr) {
+    if (typeof num !== 'number') return console.error(`El valor "${num}" ingresado, NO es un número`);
+  }
+
+  return console.info(
+    arr.reduce((total, num, index, arr) => {
+      total += num;
+      if (index === arr.length - 1) { // -1 porque length es el numero de elementos y las posiciones empiezan desde 0
+        return `El promedio de ${arr.join(' + ')} es ${total / arr.length}`;
+      } else {
+        return total;
+      }
+    })
+  )
+}
+
+promedio();
+promedio({});
+promedio([]);
+promedio([2, true]);
+promedio([1,2,3,4,5,6,7,8,9,0]);
+
+
+
+
+
+
+
+
+
+
+
+// 27) Programa una clase llamada Pelicula.
+
+// La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
+//   - Todos los datos del objeto son obligatorios.
+//   - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los
+//      7 restantes números.
+//   - Valida que el título no rebase los 100 caracteres.
+//   - Valida que el director no rebase los 50 caracteres.
+//   - Valida que el año de estreno sea un número entero de 4 dígitos.
+//   - Valida que el país o paises sea introducidos en forma de arreglo.
+//   - Valida que los géneros sean introducidos en forma de arreglo.
+//   - Valida que los géneros introducidos esten dentro de los géneros
+//      aceptados*.
+//   - Crea un método estático que devuelva los géneros aceptados*.
+//   - Valida que la calificación sea un número entre 0 y 10 pudiendo ser
+//     decimal de una posición.
+//   - Crea un método que devuelva toda la ficha técnica de la película.
+//   - Apartir de un arreglo con la información de 3 películas genera 3
+//     instancias de la clase de forma automatizada e imprime la ficha técnica
+//     de cada película.
+
+// * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western.
