e59a56c863f23bf512718db2d5a8ff9291e5a16a
Damian <damiandanielvigo@gmail.com>
2020-06-02 19:37:41 -0300

    dom++

--- otros/hamburger-button/js/index.js
+++ otros/hamburger-button/js/index.js
@@ -6,6 +6,7 @@ const d = document;
 d.addEventListener('DOMContentLoaded', (e) => {
   hamburguerMenu('.panel-btn', '.panel', '.menu a');
   digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
+  alarm('assets/end-of-time-alan-walker.ogg', '#activar-alarma', '#desactivar-alarma')
 });
 
 
