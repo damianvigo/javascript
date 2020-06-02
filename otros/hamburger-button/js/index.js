0eb194b5474171d458cdd8a6d3fd8c68f603d610
Damian <damiandanielvigo@gmail.com>
2020-06-02 05:43:35 -0300

    remember-react

--- otros/hamburger-button/js/index.js
+++ otros/hamburger-button/js/index.js
@@ -1,7 +1,11 @@
 import hamburguerMenu from './modules/menu_hamburguesa.js';
+import { digitalClock, alarm } from './modules/reloj.js';
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
   hamburguerMenu('.panel-btn', '.panel', '.menu a');
+  digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj')
 });
+
+
