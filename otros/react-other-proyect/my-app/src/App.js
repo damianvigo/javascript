f6c657ffd6b38e5cb7dd8e7c84997d0269d382af
Damian <damiandanielvigo@gmail.com>
2020-06-04 05:35:18 -0300

    update-react

--- otros/react-other-proyect/my-app/src/App.js
+++ otros/react-other-proyect/my-app/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 import tasks from './sample/tasks.json';
@@ -6,6 +8,7 @@ import tasks from './sample/tasks.json';
 // Components
 import Tasks from './components/Tasks';
 import TaskForm from './components/TaskForm';
+import Posts from './components/Posts';
 
 class App extends Component {
   state = {
@@ -23,12 +26,46 @@ class App extends Component {
     });
   };
 
+  deleteTask = (id) => {
+    const newTasks = this.state.tasks.filter((task) => task.id !== id);
+    this.setState({
+      tasks: newTasks,
+    });
+  };
+
+  checkDone = (id) => {
+    const newTasks = this.state.tasks.map((task) => {
+      if (task.id === id) {
+        task.done = !task.done;
+      }
+      return task;
+    });
+    this.setState({
+      tasks: newTasks,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <TaskForm addTask={this.addTask} />
-        <Tasks tasks={this.state.tasks} />
-      </div>
+      <Router>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to='/posts'>Posts</Link>
+
+        <Route
+          exact
+          path='/'
+          render={() => {
+            return (
+              <div>
+                <TaskForm addTask={this.addTask} />
+                <Tasks tasks={this.state.tasks} deleteTask={this.deleteTask} checkDone={this.checkDone} />
+              </div>
+            );
+          }}
+        ></Route>
+        <Route path='/posts' component={Posts} />
+      </Router>
     );
   }
 }
--- otros/react-other-proyect/my-app/src/components/Task.js
+++ otros/react-other-proyect/my-app/src/components/Task.js
@@ -18,8 +18,10 @@ class Task extends Component {
       <p style={this.StyleCompleted()}>
         {/* {this.props.task.title} - {this.props.task.description} - {this.props.task.done} - {this.props.task.id} */}
         {task.title} - {task.description} - {task.done} - {task.id}
-        <input type='checkbox' />
-        <button style={btnDelete}>x</button>
+        <input type='checkbox' onChange={this.props.checkDone.bind(this, task.id)} />
+        <button style={btnDelete} onClick={this.props.deleteTask.bind(this, task.id)}>
+          x
+        </button>
       </p>
     );
   }
