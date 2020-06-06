e480b2835c495970f2c1e232ccdc2628d26d192a
Damian <damiandanielvigo@gmail.com>
2020-06-05 22:05:47 -0300

    dom++

--- otros/hamburger-button/js/index.js
+++ otros/hamburger-button/js/index.js
@@ -1,6 +1,7 @@
 import hamburguerMenu from './modules/menu_hamburguesa.js';
 import { digitalClock, alarm } from './modules/reloj.js';
 import { moveBall, shortcuts } from './modules/teclado.js';
+import countdown from './modules/cuenta_regresiva.js';
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
   hamburguerMenu('.panel-btn', '.panel', '.menu a');
   digitalClock('#reloj', '#activar-reloj', '#desactivar-reloj');
   alarm('assets/end-of-time-alan-walker.ogg', '#activar-alarma', '#desactivar-alarma');
+  countdown('countdown', 'dic 25, 2020 00:00:00', 'Merry Christmas');
 });
 
 d.addEventListener('keydown', (e) => {
