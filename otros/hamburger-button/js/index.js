2155e6cffb52050df62aac508bf5043c114d4d87
Damian <damiandanielvigo@gmail.com>
2020-06-10 23:48:52 -0300

    dom++

--- otros/hamburger-button/js/index.js
+++ otros/hamburger-button/js/index.js
@@ -3,6 +3,7 @@ import { digitalClock, alarm } from './modules/reloj.js';
 import { moveBall, shortcuts } from './modules/teclado.js';
 import countdown from './modules/cuenta_regresiva.js';
 import scrollTopButton from './modules/boton_scroll.js';
+import darkTheme from './modules/tema_oscuro.js';
 
 const d = document;
 
@@ -12,6 +13,7 @@ d.addEventListener('DOMContentLoaded', (e) => {
   alarm('assets/end-of-time-alan-walker.ogg', '#activar-alarma', '#desactivar-alarma');
   // countdown('countdown', 'dic 25, 2020 00:00:00', 'Merry Christmas');
   scrollTopButton('.scroll-top-btn');
+  darkTheme('.dark-theme-btn', 'dark-mode');
 });
 
 d.addEventListener('keydown', (e) => {
