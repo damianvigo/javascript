36c0d52c8eeb67f7b711cb357706bce8b27b9864
Damian <damiandanielvigo@gmail.com>
2020-09-17 15:28:24 -0300

    pay-stripe-rdy

--- ajax/ajax-ejercicios/assets/stripe-checkout.js
+++ ajax/ajax-ejercicios/assets/stripe-checkout.js
@@ -1,3 +1,4 @@
+import stripeKeys from './stripe-keys.js';
 import STRIPE_KEYS from './stripe-keys.js';
 // console.log(STRIPE_KEYS);
 
@@ -13,6 +14,10 @@ const d = document,
   };
 
 let products, prices; // variables de inicio vacias para luego agregarles el resultado de las peticiones fetch
+
+//ultimos 2 caracteres // 9000 = $90 mxn
+// ultimos 2 valores = 90.00 mxn
+const moneyFormat = (num) => `$${num.slice(0, -2)}.${num.slice(-2)}`;
 
 Promise.all([fetch('https://api.stripe.com/v1/products', fetchOptions), fetch('https://api.stripe.com/v1/prices', fetchOptions)])
   // esperando que respondan cada uno de los endpoints y cuando responda:
@@ -33,8 +38,8 @@ Promise.all([fetch('https://api.stripe.com/v1/products', fetchOptions), fetch('h
       $template.querySelector('img').alt = productData[0].name;
       $template.querySelector('figcaption').innerHTML = `${productData[0].name}
         <br />
-        ${el.unit_amount_decimal} ${el.currency}
-      `
+        ${moneyFormat(el.unit_amount_decimal)} ${el.currency}
+      `;
 
       let $clone = d.importNode($template, true);
       $fragment.appendChild($clone);
@@ -47,3 +52,26 @@ Promise.all([fetch('https://api.stripe.com/v1/products', fetchOptions), fetch('h
     let message = err.statusText || 'Ocurrio un erro al conectarse con la API de Stripe';
     $tacos.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
   });
+
+d.addEventListener('click', (e) => {
+  console.log(e.target);
+  /* '.taco *' cualquier cosa que este dentro de taco, es decir a la img o tambien al figcaption. A los elementos internos de la clase taco */
+  if (e.target.matches('.taco *')) {
+    // alert('A comprar');
+    let price = e.target.parentElement.getAttribute('data-price');
+    // console.log(price);
+    Stripe(STRIPE_KEYS.public)
+      .redirectToCheckout({
+        lineItems: [{ price, quantity: 1 }],
+        mode: 'subscription',
+        successUrl: 'http://127.0.0.1:5500/ajax-ejercicios/assets/stripe-success.html',
+        cancelUrl: 'http://127.0.0.1:5500/ajax-ejercicios/assets/stripe-cancel.html',
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.error) {
+          $tacos.insertAdjacentHTML('afterend', res.error.message);
+        }
+      });
+  }
+});
