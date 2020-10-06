c393c626362e0ad6cf8b283a109cba53ab063c37
Damian <damiandanielvigo@gmail.com>
2020-10-06 12:14:14 -0300

    store-jugadores

--- otros/app-manager/src/App.js
+++ otros/app-manager/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import Jugadores from './components/Jugadores';
 
 const App = () => (
-  <main>
-    <h1>App-Manager</h1>
-    <Jugadores />
-    <EquipoSeleccionado />
-  </main>
+  <Provider store={store}>
+    <main>
+      <h1>App-Manager</h1>
+      <Jugadores />
+      {/*  <EquipoSeleccionado /> */}
+    </main>
+  </Provider>
 );
 
 export default App;
--- otros/app-manager/src/store.js
+++ otros/app-manager/src/store.js
@@ -1,3 +1,29 @@
-import { createStore } from 'redux'
+import { createStore } from 'redux';
 
-export default createStore()
+const initialState = {
+  jugadores: [
+    {
+      id: 1,
+      nombre: 'Cristiano Ronaldo',
+      foto: 'https://as.com/tikitakas/imagenes/2020/10/05/portada/1601919185_245348_1601919417_noticia_normal_recorte1.jpg',
+    },
+    {
+      id: 2,
+      nombre: 'Wesley Sneijder',
+      foto: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Wesley2012.JPG',
+    },
+    {
+      id: 3,
+      nombre: 'Toni Kroos',
+      foto: 'https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2020/04/09/toni-kroos.jpeg',
+    },
+  ],
+  titulares: [],
+  suplentes: [],
+};
+
+const reducerEntrenador = (state = initialState, action) => {
+  return state;
+};
+
+export default createStore(reducerEntrenador);
