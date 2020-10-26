a9d9e59a69d9f241c89e88fa7b4e34778b4cc349
Damian <damiandanielvigo@gmail.com>
2020-10-26 20:30:17 -0300

    search-videos-rdy

---
+++ otros/search-movies/search-movies/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { NotFound } from './pages/NotFound'
+
+import 'bulma/css/bulma.css'
+
+class App extends Component {
+  render() {
+    /*   const url = new URL(document.location)
+      const Page = url.searchParams.has('id')
+        ? <Detail id={url.searchParams.get('id')} />
+        : <Home /> */
+
+    return (
+      <div className="App">
+        {/* {Page} */}
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/detail/:id' component={Detail} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    );
+  }
+
+}
+
+export default App;
