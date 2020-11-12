b5399d405fcb635b3d50706dd184779e4dc54a9f
Damian <damiandanielvigo@gmail.com>
2020-11-12 16:32:24 -0300

    add-helper-ajax

--- otros/spa/app/App.js
+++ otros/spa/app/App.js
@@ -1,7 +1,19 @@
 import api from './helpers/wp_api.js'
+import { ajax } from './helpers/ajax.js'
 
 export function App() {
   document.getElementById('root').innerHTML = `<h1>SPA</h1>`;
 
+  ajax({
+    url: api.POSTS,
+    cbSuccess: (posts) => {
+      console.log(posts);
+    }
+  });
   console.log(api)
 }
+
+  ajax({
+    url: api.CATEGORIES,
+    cbSuccess: (categories) => console.log(categories)
+  })
--- otros/spa/app/helpers/wp_api.js
+++ otros/spa/app/helpers/wp_api.js
@@ -4,6 +4,7 @@ SITE = `${DOMAIN}/wp-json`,
 API_WP = `${SITE}/wp/v2`, // api nativa de wordpress. informacion nativa de todo sitio en wordpress
 POSTS = `${API_WP}/posts?_embed`,
 POST = `${API_WP}/posts`,
+CATEGORIES = `${API_WP}/categories`,
 SEARCH = `${API_WP}/search?_embed&search=`;
 
 export default {
@@ -13,5 +14,6 @@ export default {
   API_WP,
   POSTS,
   POST,
-  SEARCH
+  SEARCH,
+  CATEGORIES
 }
