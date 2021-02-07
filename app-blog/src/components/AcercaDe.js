1ea5194d11d63cbcfb225660c8523cad1019da94
Damian <damiandanielvigo@gmail.com>
2021-02-07 15:59:29 -0300

    add-routes

---
+++ app-blog/src/components/AcercaDe.js
@@ -0,0 +1,10 @@
+const AcercaDe = () => {
+  return (
+    <div>
+      <h2>Acerca de</h2>
+      <p>Hola soy Damian Vigo frontend developer</p>
+    </div>
+  );
+};
+
+export default AcercaDe;
---
+++ app-blog/src/components/Inicio.js
@@ -0,0 +1,10 @@
+const Inicio = () => {
+  return (
+    <div>
+      <h2>Pagina de Inicio</h2>
+      <p>Esta es la pagina de inicio del sitio</p>
+    </div>
+  );
+};
+
+export default Inicio;
