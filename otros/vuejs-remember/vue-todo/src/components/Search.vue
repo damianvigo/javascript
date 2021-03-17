fd32e8f8dd5866b05ab81b372df867e835e62bd8
Damian <damiandanielvigo@gmail.com>
2021-03-17 01:02:21 -0300

    todos-vue-done

---
+++ otros/vuejs-remember/vue-todo/src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="search-container">
+    <input type="text" v-model='query' autofocus
+    v-on:input="$emit('query-change', query)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      query: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#search-container {
+  width: 50%;
+  margin: 0 auto;
+}
+
+input {
+  padding: 10px;
+  width: 100%;
+  outline: none;
+}
+</style>
+
