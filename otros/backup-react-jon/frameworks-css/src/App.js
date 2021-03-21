2febe90e026eba9f58603ad4d033852004636e0d
Damian <damiandanielvigo@gmail.com>
2021-03-21 15:42:19 -0300

    backup-07

---
+++ otros/backup-react-jon/frameworks-css/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+/* import Bulma from './components/Bulma'; */
+/* import Bootstrap from './components/Bootstrap'; */
+/* import ReactBootstrap from './components/ReactBootstrap'; */
+import MaterialUI from './components/MaterilaUI';
+
+function App() {
+  return (
+    <div>
+      <h1>Frameworks CSS con React</h1>
+      {/*  <Bootstrap /> */}
+      {/*  <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
+    </div>
+  );
+}
+
+export default App;
