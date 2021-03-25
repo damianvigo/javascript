898d8f6f6fe9f32ef552b7a59ffb85c6e3ac5f02
Damian <damiandanielvigo@gmail.com>
2021-03-24 23:50:03 -0300

    backup-08

---
+++ otros/backup-react-jon/crud-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CrudApp } from './components/CrudApp';
+
+function App() {
+  return (
+    <>
+      <h2>CRUD</h2>
+      <CrudApp />
+    </>
+  );
+}
+
+export default App;
