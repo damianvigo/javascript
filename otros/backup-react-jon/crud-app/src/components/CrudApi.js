b3bd921f52f855e88934909aac5a2211bc3fdc10
Damian <damiandanielvigo@gmail.com>
2021-04-08 20:35:23 -0300

    cr-04

--- otros/backup-react-jon/crud-app/src/components/CrudApi.js
+++ otros/backup-react-jon/crud-app/src/components/CrudApi.js
@@ -2,24 +2,33 @@ import React, { useState, useEffect } from 'react';
 import { helpHttp } from '../helpers/helpHttp';
 import { CrudForm } from './CrudForm';
 import { CrudTable } from './CrudTable';
+import Loader from './Loader';
+import Message from './Message';
 
 const CrudApi = () => {
-  const [db, setDb] = useState([]);
+  const [db, setDb] = useState(null);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   let api = helpHttp();
   let url = 'http://localhost:5000/santos';
 
   useEffect(() => {
-    api.get(url).then((res) => {
+    setLoading(true);
+    helpHttp().get(url).then((res) => {
       // console.log(res);
       if (!res.err) {
         setDb(res);
+        setError(null);
       } else {
         setDb(null);
+        setError(res);
       }
+
+      setLoading(false);
     });
-  }, []);
+  }, [url]);
 
   const createData = (data) => {
     // console.log(data);
@@ -48,7 +57,9 @@ const CrudApi = () => {
       <h2>CRUD Api</h2>
       <article className='grid-1-2'>
         <CrudForm createData={createData} updateData={updateData} dataToEdit={dataToEdit} setDataToEdit={setDataToEdit} />
-        <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />
+        {loading && <Loader />}
+        {error && <Message msg={`Error ${error.status}: ${error.statusText}`} bgColor='#dc3545' />}
+        {db && <CrudTable data={db} setDataToEdit={setDataToEdit} deleteData={deleteData} />}
       </article>
     </div>
   );
