0ff9f391ec0058adbd1dd5bb63e3f5130e2a11b8
Damian <damiandanielvigo@gmail.com>
2021-04-27 23:54:54 -0300

    add-vf-3

--- otros/backup-react-jon/crud-app/src/components/ContactForm.js
+++ otros/backup-react-jon/crud-app/src/components/ContactForm.js
@@ -10,9 +10,30 @@ const initialForm = {
 
 const validationsForm = (form) => {
   let errors = {};
+  let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+  let regexComments = /^.{1,255}$/;
 
   if (!form.name.trim()) {
     errors.name = "*El campo 'Nombre' es requerido";
+  } else if (!regexName.test(form.name.trim())) {
+    errors.name = "El campo 'Nombre' solo acepta letras y espacios en blanco";
+  }
+
+  if (!form.email.trim()) {
+    errors.email = "*El campo 'Email' es requerido";
+  } else if (!regexEmail.test(form.name.trim())) {
+    errors.email = "El campo 'Email' es incorrecto";
+  }
+
+  if (!form.subject.trim()) {
+    errors.subject = "*El campo 'Asunto a tratar' es requerido";
+  }
+
+  if (!form.comments.trim()) {
+    errors.comments = "*El campo 'Comentarios' es requerido";
+  } else if (!regexComments.test(form.comments.trim())) {
+    errors.comments = "El campo 'Comentarios' no puede ser mas de 255 caracteres"
   }
 
   return errors;
@@ -54,7 +75,7 @@ const ContactForm = () => {
           value={form.comments}
           required
         ></textarea>
-          {errors.comments && <p style={styles}>{errors.comments}</p>}
+        {errors.comments && <p style={styles}>{errors.comments}</p>}
         <input type='submit' value='Enviar' />
       </form>
     </div>
