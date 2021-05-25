087b459df4744a7994c4ca47aa2c04411b65baae
Damian <damiandanielvigo@gmail.com>
2021-05-24 22:57:52 -0300

    add-components

--- otros/backup-react-jon/router/src/App.js
+++ otros/backup-react-jon/router/src/App.js
@@ -1,10 +1,18 @@
-import ConceptosBasicos from "./components/ConceptosBasicos";
+import ConceptosBasicos from './components/ConceptosBasicos';
+import CrudApi from './components/CrudApi';
+import SongSearch from './components/SongSearch';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>React Router</h1>
-      <a href="https://reactrouter.com/web/guides/quick-start" target='_blank' rel='noreferrer'>Documentacion</a>
+      <a href='https://reactrouter.com/web/guides/quick-start' target='_blank' rel='noreferrer'>
+        Documentacion
+      </a>
+      <hr />
+      <SongSearch />
+      <hr />
+      <CrudApi />
       <hr />
       <ConceptosBasicos />
     </div>
--- otros/backup-react-jon/router/src/components/ConceptosBasicos.js
+++ otros/backup-react-jon/router/src/components/ConceptosBasicos.js
@@ -16,16 +16,23 @@ const ConceptosBasicos = () => {
     <div>
       <h2>Hash Router</h2>
       <HashRouter>
-        <nav>
-          <Link to='/'>Home</Link>
-          {/* <Link to='/#/'>Acerca</Link> */}
-          <Link to='/acerca'>Acerca</Link>
-          <Link to='/contacto'>Contacto</Link>
-        </nav>
+        <MenuConceptos />
         <Switch>
-        <Route exact path='/' component={Home} />
+          <Route exact path='/' component={Home} />
           <Route exact path='/acerca' component={Acerca} />
           <Route exact path='/contacto' component={Contacto} />
+          <Route exact path='/usuario/:username' component={Usuario} />
+          <Route exact path='/productos' component={Productos} />
+          <Route exact path='/about'>
+            <Redirect to='/acerca' />
+          </Route>
+          <Route exact path='/contact'>
+            <Redirect to='/contacto' />
+          </Route>
+          <Route path='/react' component={ReactTopics} />
+          <Route exact path='/login' component={Login} />
+          {/* <Route exact path='/dashboard' component={Dashboard} /> */}
+          <PrivateRoute exact path='/dashboard' component={Dashboard} />
           <Route path='*' component={Error404} />
         </Switch>
       </HashRouter>
