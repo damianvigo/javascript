620e54a7ee9628d4f311747bf1ea20d2d0690841
Damian Vigo <damiandanielvigo@gmail.com>
2021-07-07 23:57:14 -0300

    add-reducer-01

--- otros/backup-react-jon/reducer/src/components/Contador.js
+++ otros/backup-react-jon/reducer/src/components/Contador.js
@@ -2,11 +2,19 @@ import { useReducer, useState } from "react";
 
 const initialState = { contador: 0 };
 
+const TYPES = {
+  INCREMENT: "INCREMENT",
+  INCREMENT_5: "INCREMENT_5",
+  DECREMENT: "DECREMENT",
+  DECREMENT_5: "DECREMENT_5",
+  RESET: "RESET",
+};
+
 function reducer(state, action) {
   switch (action.type) {
-    case "INCREMENT":
+    case TYPES.INCREMENT:
       return { contador: state.contador + 1 };
-    case "DECREMENT":
+    case TYPES.DECREMENT:
       return { contador: state.contador - 1 };
     default:
       return state;
@@ -27,8 +35,11 @@ const Contador = () => {
     <div style={{ textAlign: "center" }}>
       <h2>Contador</h2>
       <nav>
+        <button>+5</button>
         <button onClick={sumar}>+</button>
+        <button>0</button>
         <button onClick={restar}>-</button>
+        <button>-5</button>
       </nav>
       <h3>{state.contador}</h3>
     </div>
