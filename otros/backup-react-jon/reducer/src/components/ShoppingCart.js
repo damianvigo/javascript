1d4442e37dfc172b2c36bf24c6db987053b4eddf
Damian Vigo <damiandanielvigo@gmail.com>
2021-07-13 14:39:15 -0300

    add-reducer-04

---
+++ otros/backup-react-jon/reducer/src/components/ShoppingCart.js
@@ -0,0 +1,13 @@
+const ShoppingCart = () => {
+  return (
+    <div>
+      <h2>Carrito de Compras</h2>
+      <h3>Productos</h3>
+      <article className="box"></article>
+      <h3>Carrito</h3>
+      <article className="box"></article>
+    </div>
+  );
+};
+
+export default ShoppingCart;
