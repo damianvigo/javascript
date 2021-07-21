cd9f1593001bdccf86cd93e2b60769236b8a5c6c
Damian Vigo <damiandanielvigo@gmail.com>
2021-07-21 19:38:56 -0300

    add-reducer-08

--- otros/backup-react-jon/reducer/src/components/CartItem.js
+++ otros/backup-react-jon/reducer/src/components/CartItem.js
@@ -6,7 +6,9 @@ const CartItem = ({ data, delFromCart }) => {
       <h5>
         ${price}.00 x {quantity} = ${price * quantity}.00
       </h5>
-      <button>Eliminar</button>
+      <button onClick={() => delFromCart(id)}>Eliminar Uno</button>
+      <br />
+      <button onClick={() => delFromCart(id, true)}>Eliminar Todos</button>
     </div>
   );
 };
