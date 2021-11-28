646d2ebf743e9073ba60f5191778f20fb7bb4d3a
Damian Vigo <damiandanielvigo@gmail.com>
2021-11-27 21:41:48 -0300

    part-2

--- nodejs-jm/01ModulosCore/04-streams.js
+++ nodejs-jm/01ModulosCore/04-streams.js
@@ -14,3 +14,17 @@ Streams
 */
 
 'use strict';
+
+const fs = require('fs'),
+  readStream = fs.createReadStream('assets/nombres.txt'),
+  writeStream = fs.createWriteStream('assets/nombres_copia.txt');
+
+readStream.pipe(writeStream);
+
+readStream.on('data', function (chunk) {
+  console.log('reading:', chunk.length, 'caracteres');
+});
+
+readStream.on('end', function () {
+  console.log('Finished to read');
+});
