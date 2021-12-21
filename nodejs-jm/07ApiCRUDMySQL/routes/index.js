d59963f0bc4185db062cc43e79bc503745afeb82
Damian Vigo <damiandanielvigo@gmail.com>
2021-12-21 13:04:51 -0300

    p-16

--- nodejs-jm/07ApiCRUDMySQL/routes/index.js
+++ nodejs-jm/07ApiCRUDMySQL/routes/index.js
@@ -1,4 +1,5 @@
 'use strict';
+// API CRUD MySQL
 // controlador
 
 const movies = require('../models/movies'),
@@ -18,23 +19,113 @@ function error404(req, res, next) {
 
   next();
 }
-
+// controlador usando internamente sql, no estoy usando mvc
 router
   .use(movies)
   .get('/', (req, res, next) => {
     req.getConnection((err, movies) => {
       movies.query('SELECT * FROM movie', (err, rows) => {
-        let locals = {
-          title: 'Lista de Peliculas',
-          data: rows,
-        };
-        res.render('index', locals);
+        if (err) {
+          next(new Error('No hay registros de Peliculas'));
+        } else {
+          let locals = {
+            title: 'Lista de Peliculas',
+            data: rows,
+          };
+          // console.log(rows);
+          res.render('index', locals);
+        }
       });
     });
     // next();
   })
   .get('/agregar', (req, res, next) => {
     res.render('add-movie', { title: 'Agregar Pelicula' });
+  })
+  .post('/', (req, res, next) => {
+    // lo que viene del formulario
+    req.getConnection((err, movies) => {
+      // console.log(req.body);
+      let movie = {
+        movie_id: req.body.movie_id,
+        title: req.body.title,
+        release_year: req.body.release_year,
+        rating: req.body.rating,
+        image: req.body.image,
+      };
+
+      // console.log(movie);
+      // comodin ? pasa a ser movie
+      movies.query('INSERT INTO movie SET ?', movie, (err, rows) => {
+        return err ? next(new Error('Error al insertar')) : res.redirect('/');
+      });
+    });
+  })
+  .get('/editar/:movie_id', (req, res, next) => {
+    let movie_id = req.params.movie_id;
+    console.log(movie_id);
+
+    req.getConnection((err, movies) => {
+      // comodin ? pasa a ser movie_id
+      movies.query(
+        'SELECT * FROM movie WHERE movie_id = ?',
+        movie_id,
+        (err, rows) => {
+          console.log(err, '---', rows);
+          if (err) {
+            next(new Error('Registro No Encontrado'));
+          } else {
+            let locals = {
+              title: 'Editar Pelicula',
+              data: rows,
+            };
+            res.render('edit-movie', locals);
+          }
+        }
+      );
+    });
+  })
+  .post('/actualizar/:movie_id', (req, res, next) => {
+    req.getConnection((err, movies) => {
+      // lo que viene del formulario
+      let movie = {
+        movie_id: req.body.movie_id,
+        title: req.body.title,
+        release_year: req.body.release_year,
+        rating: req.body.rating,
+        image: req.body.image,
+      };
+
+      // console.log(movie);
+      // 2 comodines - primer comodin todo el obj movie, mas de 2 obj usar un array
+      movies.query(
+        'UPDATE movie SET ? WHERE movie_id = ?',
+        [movie, movie.movie_id],
+        (err, rows) => {
+          return err
+            ? next(new Error('Error al actualizar'))
+            : res.redirect('/');
+        }
+      );
+    });
+  })
+  .post('/eliminar/:movie_id', (req, res, next) => {
+    let movie_id = req.params.movie_id;
+    console.log(movie_id);
+
+    req.getConnection((err, movies) => {
+      // comodin ? pasa a ser movie_id
+      movies.query(
+        'DELETE FROM movie WHERE movie_id = ?',
+        movie_id,
+        (err, rows) => {
+          console.log(err, '---', rows);
+          return err
+            ? next(new Error('Registro No Encontrado'))
+            : res.redirect('/');
+        }
+      );
+    });
   })
   .use(error404);
 
