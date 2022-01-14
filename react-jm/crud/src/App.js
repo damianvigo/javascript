673cf8566e61f3350cb2fe191bfe51178375ab1e
Damian Vigo <damiandanielvigo@gmail.com>
2022-01-13 22:14:10 -0300

    select-jm

--- react-jm/crud/src/App.js
+++ react-jm/crud/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
+import SelectsAnidados from './components/SelectsAnidados';
+import SongSearch from './components/SongSearch';
 
 function App() {
   return (
     <>
       <h2>Practicas con React</h2>
+      <hr />
+      <SelectsAnidados />
+      <hr />
+      <SongSearch />
       <hr />
       <CrudApi />
       <hr />
