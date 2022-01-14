f031a42ff520f906ce31d71af1c06956bfdefab3
Damian Vigo <damiandanielvigo@gmail.com>
2022-01-14 19:51:12 -0300

    use-form-jm

--- react-jm/crud/src/App.js
+++ react-jm/crud/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContactForm from './components/ContactForm';
 import CrudApi from './components/CrudApi';
 import CrudApp from './components/CrudApp';
 import SelectsAnidados from './components/SelectsAnidados';
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <h2>Practicas con React</h2>
+      <hr />
+      <ContactForm />
       <hr />
       <SelectsAnidados />
       <hr />
