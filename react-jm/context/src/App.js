314cba50737044df4578081d357a483c9033d988
Damian Vigo <damiandanielvigo@gmail.com>
2022-01-21 15:58:05 -0300

    jmp-9

--- react-jm/context/src/App.js
+++ react-jm/context/src/App.js
@@ -1,9 +1,18 @@
+import CrudApi from './components/CrudApi';
 import MyPage from './components/MyPage';
+import MyPageContext from './components/MyPageContext';
+import { CrudProvider } from './context/CrudContext';
 
 function App() {
   return (
     <div className="App">
       <h1>React Context API</h1>
+      <hr />
+      <CrudProvider>
+        <CrudApi />
+      </CrudProvider>
+      <hr />
+      <MyPageContext />
       <hr />
       <MyPage />
     </div>
