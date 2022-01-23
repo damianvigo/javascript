436a2e97b447b2fe31a6478b09a33a2650b12f88
Damian Vigo <damiandanielvigo@gmail.com>
2022-01-22 21:20:05 -0300

    jmp-10

---
+++ react-jm/reducers/src/App.js
@@ -0,0 +1,16 @@
+import Contador from './components/Contador';
+import ContadorMejorado from './components/ContadorMejorado';
+
+function App() {
+  return (
+    <div>
+      <h1>useReducer</h1>
+      <hr />
+      <ContadorMejorado />
+      <hr />
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
